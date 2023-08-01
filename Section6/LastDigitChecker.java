fbf85c93a35c5868f13bb3888a80d5679355791f
GiannisTsevrenis <ts3v@outlook.com>
2023-08-02 00:14:54 +0300

    Add files via upload

---
+++ Section6/LastDigitChecker.java
@@ -0,0 +1,12 @@
+package Section6;
+public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int x, int y, int z){
+        if (!isValid(x) || !isValid(y) || !isValid(z)){
+            return false;
+        }
+        return (x % 10 == y % 10) || (x % 10 == z % 10) || (y % 10 == z % 10);
+    }
+    public static boolean isValid(int number){
+        return number >= 10 && number <= 1000;
+    }
+}
