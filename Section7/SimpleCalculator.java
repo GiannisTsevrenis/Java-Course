214a052f97978858a942a04f953aa16679f2e925
GiannisTsevrenis <ts3v@outlook.com>
2023-08-02 12:51:30 +0300

    Add files via upload

---
+++ Section7/SimpleCalculator.java
@@ -0,0 +1,35 @@
+package Section7;
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber (){
+        return this.firstNumber;
+    }
+    public double getSecondNumber (){
+        return this.secondNumber;
+    }
+    public void setFirstNumber(double number){
+        this.firstNumber = number;
+    }
+    public void setSecondNumber(double number){
+        this.secondNumber = number;
+    }
+    public double getAdditionResult (){
+        return this.secondNumber + this.firstNumber;
+    }
+    public double getSubtractionResult  (){
+        return this.firstNumber - this.secondNumber;
+    }
+    public double getMultiplicationResult   (){
+        return this.firstNumber * this.secondNumber;
+    }
+    public double getDivisionResult (){
+        if (this.secondNumber == 0){
+            return 0;
+        }else{
+            return this.firstNumber / this.secondNumber;
+        }
+    }
+
+}
