65ea09484aa901652cddf3b4422b9453a941ba99
GiannisTsevrenis <ts3v@outlook.com>
2023-11-07 19:46:53 +0200

    Create ListItem.java

---
+++ Section11/ListItem.java
@@ -0,0 +1,28 @@
+package Section11;
+public abstract class ListItem {
+    protected ListItem leftLink;
+    protected ListItem rightLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    abstract ListItem next();
+
+    abstract ListItem setNext(ListItem item);
+
+    abstract ListItem previous();
+
+    abstract ListItem setPrevious(ListItem item);
+
+    abstract int compareTo(ListItem item);
+}
